f7f54dccae5d12b8d4fa7c64f0f2bb58e05a7b25
[AmjadAlmutairi] <[mutairi.amjad@gmail.com]>
2017-09-12 20:29:47 +0300

    Share button on about page is completed.

--- app/src/main/java/me/amjadalmutairi/almustaghfirin/AboutActivity.java
+++ app/src/main/java/me/amjadalmutairi/almustaghfirin/AboutActivity.java
@@ -22,6 +22,8 @@ public class AboutActivity extends AppCompatActivity {
     TextView devTextView;
     @BindView(R.id.source_code_view)
     TextView sourceTextView;
+    @BindView(R.id.shareBut)
+    ImageButton share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +50,19 @@ public class AboutActivity extends AppCompatActivity {
                 Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                         "mailto","mutairi.amjad@gmail.com", null));
                 startActivity(Intent.createChooser(emailIntent, "Send email..."));
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String packageName = getApplicationContext().getPackageName();
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/html");
+                String shareBody = getString(R.string.share_message) + "\n" + "https://play.google.com/store/apps/details?id=" + packageName ;
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.share_title) + " " + getString(R.string.app) + " " + getString(R.string.app_name));
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, getString(R.string.share_title)));
             }
         });
     }
